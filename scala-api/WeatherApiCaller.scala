8d40d851ab2247815b9aa895e534c7e6b469378b
Ninad Gawad <ninadgawad@gmail.com>
2024-01-13 22:58:41 -0500

    Update WeatherApiCaller.scala

--- scala-api/WeatherApiCaller.scala
+++ scala-api/WeatherApiCaller.scala
@@ -1,14 +1,17 @@
 package com.ninad
-import com.rockthejvm.ConfigReader.readApiKey
+import com.ninad.ConfigReader.readApiKey
 
 import java.net.{HttpURLConnection, URL}
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
+
 object WeatherApiCaller {
-  def main(args: Array[String]): Unit = {
+
+
+  private def getWeatherInfo(city: String): String={
     val apiKey = readApiKey()
-    val city = "New York"
+
     val url = new URL(s"http://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=imperial")
     val connection = url.openConnection().asInstanceOf[HttpURLConnection]
     connection.setRequestMethod("GET")
@@ -18,12 +21,23 @@ object WeatherApiCaller {
       val reader = new BufferedReader(new InputStreamReader(connection.getInputStream))
       var inputLine: String = reader.readLine
       while (inputLine != null) {
+        println("Weather for City:"+city)
         println(inputLine)
         inputLine = reader.readLine
+
       }
       reader.close()
+      inputLine
     } else {
       println("Error making the request")
+      "Error"
     }
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    val city = "New York"
+    getWeatherInfo(city)
+
   }
 }
