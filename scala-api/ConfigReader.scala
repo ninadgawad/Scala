7c213b7d8a5a22c1e5ceac8fb268cb8f535c1c55
Ninad Gawad <ninadgawad@gmail.com>
2024-01-13 22:59:08 -0500

    Update ConfigReader.scala

--- scala-api/ConfigReader.scala
+++ scala-api/ConfigReader.scala
@@ -6,7 +6,7 @@ import java.io.{File, FileInputStream}
 
 object ConfigReader {
   def readApiKey(): String = {
-    val configFile = "C:\\MyData\\Ninad\\Learning\\scala-at-light-speed-start\\src\\main\\resources\\config.yml"
+    val configFile = "config.yml"
     val inputStream =  new FileInputStream(new File(configFile))
     val yaml = new Yaml(new Constructor(classOf[OpenWeatherMap]))
     val e = yaml.load(inputStream).asInstanceOf[OpenWeatherMap]
